5d056cfba82a99a8824fde00a92c6709b1ca9b81
William Wassmann <wmwassmann@hotmail.com>
2021-09-05 11:53:48 -0700

    initial push to simple classes and understandings of C#. Learned about libraries, solutions, and simple debugging.

---
+++ IntroUI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using IntroLibrary;
+
+namespace IntroUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel p = new PersonModel
+            {
+                FirstName = "William",
+                LastName = "Wassmann"
+            };
+
+            System.Console.WriteLine($"{ p.FirstName } {p.LastName } is my name");
+            System.Console.WriteLine(p.FirstName);
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is a test");
+            Console.WriteLine("This is the end");
+        }
+    }
+}
+    
